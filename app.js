cc88bafb26d82a1a1fed71742b7d41be69f9499a
44ys47pgvm-dot <44ys47pgvm@privaterelay.appleid.com>
2026-09-07 04:05:49 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://psyqffckpcajzdzkcboh.supabase.co";
 const SUPABASE_KEY = "sb_publishable_Npm2bjIqxtACscbdjxHbFA_NCqknFxv";
 
 const { createClient } = supabase;
-const db = createClient(SUPABASE_URL, SUPABASE_KEY);
+const db = createClient(,https://psyqffckpcajzdzkcboh.supabase.cosb_publishable_Npm2bjIqxtACscbdjxHbFA_NCqknFxv);
 
 const output = document.getElementById("output");
 
